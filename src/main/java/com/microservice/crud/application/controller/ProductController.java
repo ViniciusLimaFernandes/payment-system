12f25cdc60ffd7153feee1c7d67d20be2aebb5ce
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2021-03-02 19:20:11 -0300

    find all resource

--- src/main/java/com/microservice/crud/application/controller/ProductController.java
+++ src/main/java/com/microservice/crud/application/controller/ProductController.java
@@ -2,12 +2,17 @@ package com.microservice.crud.application.controller;
 
 import com.microservice.crud.application.data.ProductVO;
 import com.microservice.crud.application.service.ProductService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -27,5 +32,23 @@ public class ProductController  {
         ProductVO productVO = productService.findByID(id);
         productVO.add(linkTo(methodOn(ProductController.class).findByID(id)).withSelfRel());
         return productVO;
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<?> findAll (@RequestParam(value = "page", defaultValue = "0") int page,
+                             @RequestParam(value = "limit", defaultValue = "12") int limit,
+                             @RequestParam(value = "direction", defaultValue = "asc") String direction){
+
+        Sort.Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "nome"));
+
+        Page<ProductVO> products = productService.findAll(pageable);
+
+        products.stream().forEach(p -> p.add(linkTo(methodOn(ProductController.class).findByID(p.getId())).withSelfRel()));
+
+        PagedModel<EntityModel<ProductVO>> pagedModel = assembler.toModel(products);
+
+        return ResponseEntity.ok(pagedModel);
     }
 }
