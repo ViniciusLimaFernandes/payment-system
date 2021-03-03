7dfed5fb131f4a666a13f3773836de1862879b58
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2021-03-03 01:12:04 -0300

    CRUD exception handler

---
+++ src/main/java/com/microservice/crud/application/exceptions/ExceptionResponse.java
@@ -0,0 +1,20 @@
+package com.microservice.crud.application.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse implements Serializable {
+
+    private Date timestamp;
+
+    private String message;
+
+    private String details;
+}
